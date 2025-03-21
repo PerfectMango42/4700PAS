ded33b13e6d098e4fab12018d2878dcf20ec77d4
Adrian Osborne <aosbornejr@gmail.com>
2025-03-21 17:40:44 -0400

    PA 9

---
+++ MD/MDCode/InitVStream.m
@@ -0,0 +1,46 @@
+doPlot = 1;
+dt = 5e-15;
+TStop = 3000 * dt;
+InitDist = 0.0;
+Method = 'VE'; % VE -- verlot; FD -- Forward Difference
+
+Mass0 = 14 * C.am; % Silicon usually 14
+Mass1 = 100 * C.am; % Argon usually 100
+
+AtomSpacing = 0.5430710e-9;
+LJSigma = AtomSpacing / 2^(1 / 6);
+LJEpsilon = 1e-21;
+
+PhiCutoff = 3 * AtomSpacing * 1.1;
+
+T = 30;
+
+% (LAtoms, WAtoms, X0, Y0, VX0, VY0, InitDist, Temp, Type)
+% AddRectAtomicArray(10, 10, 0, 0, 0, 0, 0, T, 0);
+
+% (rad, X0, Y0, VX0, VY0, InitDist, Temp, Type)
+% AddCircAtomicArray(2, 4e-9, 0, 0, 0, 0, T, 0);
+% AddCircAtomicArray(2, -4e-9, 0, 0, 0, 0, T, 0);
+% vy0 = -sqrt(0.02*Ep/Mass1);
+% AddRectAtomicArray(4,4,0,12*AtomSpacing,0,vy0,0,T,1);
+
+
+% AddNewGeometry (size, X0, Y0, VX0, VY0, InitDist, Temp, Type)
+AddNewGeometry(5, 0, 0, 0, 0, 0, T, 0);
+%AddRectAtomicArray(10, 10, 0, 0, 0, 0, 0, T, 0);
+
+Ep = 30;
+AddParticleStream(1, 0.1, 10, -pi / 2, 1, Ep * C.q_0, 5);
+AddParticleStream(1, 0.1, -10, pi / 2, 1, Ep * C.q_0, 5);
+
+Size = 10*AtomSpacing;
+Limits = [-Size +Size -Size +Size]; % square is good
+PlDelt = 5 * dt;
+
+PlotFile = 'BlockSt.gif';
+PlotPosOnly = 1;
+doPlotImage = 0;
+PlotSize = [100, 100, 1049, 1049];
+
+ScaleV = .02e-11;
+ScaleF = 10;
